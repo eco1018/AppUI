b3acfab1ecf26234b1d16951659e069f1263ff45
eco1018 <dealasadduq@gmail.com>
2025-05-31 14:07:38 +0700

    Initial Commit

---
+++ AppUITests/AppUITests.swift
@@ -0,0 +1,17 @@
+//
+//  AppUITests.swift
+//  AppUITests
+//
+//  Created by Ella A. Sadduq on 5/31/25.
+//
+
+import Testing
+@testable import AppUI
+
+struct AppUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
